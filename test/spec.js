afb99388b399d4abadb51d65dafbda3341ecf3c1
xyshtd <mhu225@gmail.com>
2022-10-17 23:50:45 -0400

    db tests & api tests

--- test/spec.js
+++ test/spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const db = require('../db');
 const supertest = require('supertest');
 const app = supertest(require('../app'));
-
+// Using mocha and chai write one test in your spec.js file
 describe('addition', ()=> {
   describe('1 + 1', ()=> {
     it('equals 2', ()=> {
@@ -10,19 +10,20 @@ describe('addition', ()=> {
     });
   });
 });
-/* 
+
 describe('My App', ()=> {
+  //In your test file, write a before each which runs syncAndSeed
   beforeEach(async()=> {
     await db.syncAndSeed();
   });
-  it('can sync', ()=> {
-
-  });
+  it('can sync', ()=> {});
+  //db model test
   it('there are 5 actors', async()=> {
     const actors = await db.models.Actor.findAll();
     expect(actors.length).to.equal(5);
 
   });
+  //Write a test using supertest which confirms that your express application has a working GET '/' route
   describe('API', ()=> {
     describe('/api/movies', ()=> {
       it('returns the movies', async()=> {
@@ -40,4 +41,3 @@ describe('My App', ()=> {
     });
   });
 });
- */
